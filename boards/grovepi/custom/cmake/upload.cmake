8a9782b76529ec94108ec2214d56bfc1e344aa7a
jasta <jasta@devtcg.org>
2022-05-30 21:07:11 -0700

    Added my GrovePi+ custom build

--- boards/grovepi/custom/cmake/upload.cmake
+++ boards/grovepi/custom/cmake/upload.cmake
@@ -34,13 +34,19 @@
 if(DEFINED ENV{AUTOPILOT_HOST})
 	set(AUTOPILOT_HOST $ENV{AUTOPILOT_HOST})
 else()
-	set(AUTOPILOT_HOST "skipp3r")
+	set(AUTOPILOT_HOST "raspberrypi")
+endif()
+
+if(DEFINED ENV{AUTOPILOT_USER})
+	set(AUTOPILOT_USER $ENV{AUTOPILOT_USER})
+else()
+	set(AUTOPILOT_USER "pi")
 endif()
 
 add_custom_target(upload
 	COMMAND rsync -arh --progress
-			${CMAKE_RUNTIME_OUTPUT_DIRECTORY} ${PX4_SOURCE_DIR}/posix-configs/rpi/*.config ${PX4_BINARY_DIR}/etc # source
-			jasta@${AUTOPILOT_HOST}:/home/jasta/px4 # destination
+			${CMAKE_RUNTIME_OUTPUT_DIRECTORY} ${PX4_SOURCE_DIR}/posix-configs/rpi/grovepi*.config ${PX4_BINARY_DIR}/etc # source
+			"${AUTOPILOT_USER}@${AUTOPILOT_HOST}:/home/${AUTOPILOT_USER}/px4" # destination
 	DEPENDS px4
 	COMMENT "uploading px4"
 	USES_TERMINAL
